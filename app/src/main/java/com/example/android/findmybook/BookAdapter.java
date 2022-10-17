2c65404e913aba66415f3ae00bacfd834881dab6
Sagar <sagar.0dev@gmail.com>
2022-10-17 18:52:32 +0530

    initial version

--- app/src/main/java/com/example/android/findmybook/BookAdapter.java
+++ app/src/main/java/com/example/android/findmybook/BookAdapter.java
@@ -7,38 +7,37 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import java.util.List;
+
 import com.squareup.picasso.Picasso;
 
+import java.util.List;
 
 
 public class BookAdapter extends ArrayAdapter<Book> {
     public BookAdapter(Activity context, List<Book> books) {
-        super(context,0,books);
+        super(context, 0, books);
     }
 
     @Override
-    public View getView(int position, View convertView,ViewGroup parent) {
+    public View getView(int position, View convertView, ViewGroup parent) {
 
-        Book currentBook=getItem(position);
-        View listItemView=convertView;
+        Book currentBook = getItem(position);
+        View listItemView = convertView;
         if (listItemView == null) {
             listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
 
         }
 
-        TextView bookname=listItemView.findViewById(R.id.book_name);
+        TextView bookname = listItemView.findViewById(R.id.book_name);
         bookname.setText(currentBook.getBookName());
 
-        TextView authorname= listItemView.findViewById(R.id.author_name);
+        TextView authorname = listItemView.findViewById(R.id.author_name);
         authorname.setText(currentBook.getAuthorName());
 
-        TextView lang=listItemView.findViewById(R.id.language_view);
+        TextView lang = listItemView.findViewById(R.id.language_view);
         lang.setText(currentBook.getLanguage());
 
-        ImageView img=listItemView.findViewById(R.id.image_view);
+        ImageView img = listItemView.findViewById(R.id.image_view);
         Picasso.get().load(currentBook.getImage()).into(img);
 
         return listItemView;
