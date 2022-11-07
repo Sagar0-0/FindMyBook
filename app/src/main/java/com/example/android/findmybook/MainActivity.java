b7e49b531963fa086bb8d1758cb7c423731a0a55
Sagar <sagar.0dev@gmail.com>
2022-11-07 12:48:16 +0530

    dependency fixed

--- app/src/main/java/com/example/android/findmybook/MainActivity.java
+++ app/src/main/java/com/example/android/findmybook/MainActivity.java
@@ -1,16 +1,16 @@
-//package com.example.android.findmybook;
-//
-//import android.os.Bundle;
-//
-//import androidx.appcompat.app.AppCompatActivity;
-//
-//public class MainActivity extends AppCompatActivity {
-//
-//    private final static String GOOGLE_BOOKS_HTTP_STRING = "https://www.googleapis.com/books/v1/volumes?q=";
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//    }
-//}
+package com.example.android.findmybook;
+
+import android.os.Bundle;
+
+import androidx.activity.ComponentActivity;
+
+public class MainActivity extends ComponentActivity {
+
+    private final static String GOOGLE_BOOKS_HTTP_STRING = "https://www.googleapis.com/books/v1/volumes?q=";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
