42cca5333f911304585086ea170f56a6e5005b54
Sagar <sagar.0dev@gmail.com>
2022-12-26 21:37:58 +0530

    outlined textfield working

--- app/src/main/java/com/example/android/findmybook/presentation/ui/SearchScreen.kt
+++ app/src/main/java/com/example/android/findmybook/presentation/ui/SearchScreen.kt
@@ -1,20 +1,21 @@
 package com.example.android.findmybook.presentation.ui
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusManager
@@ -36,54 +37,67 @@ fun SearchScreen(
     var text by rememberSaveable {
         mutableStateOf(viewModel.searchTitle.value)
     }
+    var isInvalidInput by rememberSaveable {
+        mutableStateOf(false)
+    }
+    val resource by viewModel.books.observeAsState()
+
     Column(
-        modifier = Modifier
-            .fillMaxWidth()
+        modifier = Modifier.fillMaxWidth()
     ) {
-        Row(
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            TextField(
-                value = text,
-                onValueChange = {
-                    text = it
-                },
-                modifier = Modifier
-                    .weight(0.9f),
-                label = {
-                    Text("Search")
-                },
-                leadingIcon = {
-                    Icon(
-                        imageVector = Icons.Filled.Search,
-                        contentDescription = "Search"
-                    )
-                },
-                keyboardOptions = KeyboardOptions.Default.copy(
-                    imeAction = ImeAction.Search,
-                    keyboardType = KeyboardType.Text
+        OutlinedTextField(
+            value = text,
+            isError = isInvalidInput,
+            supportingText = {
+                if (isInvalidInput) {
+                    resource?.message?.let { Text(text = it) }
+                }
+            },
+            singleLine = true,
+            maxLines = 1,
+            shape = CircleShape,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    start = 10.dp,
+                    end = 10.dp
                 ),
-                keyboardActions = KeyboardActions(onSearch = {
-                    focusManager.clearFocus()
-                    onExecuteSearch(viewModel, text)
-                }),
-                textStyle = TextStyle(color = MaterialTheme.colorScheme.onSurface)
-            )
-            Button(onClick = {
-                focusManager.clearFocus()
-                onExecuteSearch(viewModel, text)
-            }
-            ) {
+            onValueChange = {
+                text = it
+            },
+            label = {
+                Text("Search")
+            },
+            trailingIcon = {
                 Icon(
                     imageVector = Icons.Filled.Search,
-                    contentDescription = "Search"
+                    contentDescription = "Search",
+                    modifier = Modifier.clickable(
+                        interactionSource = remember { MutableInteractionSource() },
+                        indication = rememberRipple(bounded = false, radius = 18.dp)
+                    ) {
+                        focusManager.clearFocus()
+                        onExecuteSearch(viewModel, text)
+                        isInvalidInput = false
+                        text=text.trim()
+                    }
                 )
-            }
-        }
+            },
+            keyboardOptions = KeyboardOptions.Default.copy(
+                imeAction = ImeAction.Search,
+                keyboardType = KeyboardType.Text
+            ),
+            keyboardActions = KeyboardActions(onSearch = {
+                focusManager.clearFocus()
+                isInvalidInput = false
+                onExecuteSearch(viewModel, text)
+                text=text.trim()
+            }),
+            textStyle = TextStyle(color = MaterialTheme.colorScheme.onSurface)
+        )
 
         Spacer(modifier = Modifier.height(8.dp))
 
-        val resource by viewModel.books.observeAsState()
         when (resource?.status) {
             Status.SUCCESS -> {
                 val books = resource?.data?.items
@@ -116,8 +130,7 @@ fun SearchScreen(
                 }
             }
             Status.ERROR -> {
-                val message = resource!!.message
-
+                isInvalidInput = true
             }
             else -> {
                 Text(text = "Search for your favourite books here")
@@ -127,7 +140,7 @@ fun SearchScreen(
 }
 
 fun onExecuteSearch(viewModel: MainViewModel, text: String) {
-    viewModel.searchBookByTitle(text)
+    viewModel.searchBookByTitle(text.trim())
 }
 
 
